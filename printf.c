128c8ab21d747bf9c73ad98c1094e5383b426bba
salawekesa <107699372+salawekesa@users.noreply.github.com>
2022-08-10 00:02:27 +0300

    Updated printf

--- printf.c
+++ printf.c
@@ -1,44 +1,66 @@
 #include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- *_printf - function to prints aaccording to format
- *@format: types of argument passed to the function
- *
- *Return: number of cs printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int check = 0, i;
-	va_list params;
-	int (*func)(va_list);
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(params, format);
-	if (!format)
+	if (format == NULL)
 		return (-1);
 
-	for (i = 0; format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (!(format[i]))
-				return (-1);
+	va_start(list, format);
 
-			func = get_func(format[i]);
-			if (!func)
-			{
-				_putchar('%');
-				_putchar(format[i]);
-				check += 2;
-			}
-			else
-				check += func(params);
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
 		{
-			_putchar(format[i]);
-			check++;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
 	}
-	va_end(params);
-	return (check);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
